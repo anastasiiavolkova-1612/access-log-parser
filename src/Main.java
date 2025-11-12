63199562019f89923f072c0c6d097d0883830774
Anastasiia Volkova <anastasiia_a.volkova@mail.ru>
2025-11-12 18:29:00 +0400

    Adding code that calculates the numbers passed as input

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String line = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + line.length());
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число:");
+        int a = scanner.nextInt();
+
+        System.out.println("Введите второе число:");
+        int b = scanner.nextInt();
+
+        // Формулы вычисления
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b;
+
+        // Вывод результатов вычисления
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
     }
 }
