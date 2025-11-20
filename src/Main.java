34255c6ac15bff8b3ce86d9769ddc81deaa2325c
Anastasiia Volkova <anastasiia_a.volkova@mail.ru>
2025-11-21 00:35:43 +0400

    Implement an infinite while loop that checks the path to a file.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String line = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + line.length());
+        Scanner scanner = new Scanner(System.in);
+        int validPathsCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isFile = file.isFile();
+
+            if (!fileExists || !isFile) {
+                System.out.println("Файл не существует или указан путь к папке");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            validPathsCount++;
+            System.out.println("Это файл номер " + validPathsCount);
+        }
     }
 }
